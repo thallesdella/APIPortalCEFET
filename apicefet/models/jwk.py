0a851437273db61c857befb0741884bf569ec03b
Thalles Koester <thallesdella@gmail.com>
2020-02-11 19:30:06 -0300

    Not permitting nullable

--- apicefet/models/jwk.py
+++ apicefet/models/jwk.py
@@ -5,8 +5,8 @@ from apicefet import db
 class Jwk(db.Model):
     _tablename__ = 'jwt_key'
     id = Column(Integer, primary_key=True)
-    key = Column(String(100), unique=True)
-    type = Column(String(10))
+    key = Column(String(100), unique=True, nullable=False)
+    type = Column(String(10), nullable=False)
 
     def __init__(self, key, type):
         self.key = key
