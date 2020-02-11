5fd94c1b016b9baa3d403db2fd667a6725cf9ff4
Thalles Koester <thallesdella@gmail.com>
2020-02-11 19:30:36 -0300

    Calling function to create db

--- apicefet/__init__.py
+++ apicefet/__init__.py
@@ -6,12 +6,13 @@ db = SQLAlchemy()
 
 
 def create_app(env='develop'):
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=False)
 
     if env == 'production':
         from configs.production_config import ProductionConfig as Config
     else:
         from configs.development_config import DevelopmentConfig as Config
+
     app.config.from_object(Config)
 
     db.init_app(app)
@@ -27,6 +28,8 @@ def create_app(env='develop'):
         app.register_blueprint(profile.blueprint, url_prefix='/perfil')
         app.register_blueprint(report.blueprint, url_prefix='/relatorios')
         app.register_blueprint(schedule.blueprint, url_prefix='/horarios')
+
+        db.create_all()
 
         app.register_error_handler(HTTPException, handle_http_exception)
 
