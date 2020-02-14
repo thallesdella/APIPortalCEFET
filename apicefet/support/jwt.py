8973cc69b7dc2d0bdd43dd9a0aa8dec78543ade6
Thalles Koester <thallesdella@gmail.com>
2020-02-14 02:17:53 -0300

    Using it's dangerous instead of jwcrypt

--- apicefet/support/jwt.py
+++ apicefet/support/jwt.py
@@ -1,38 +1,28 @@
 from apicefet.models.jwk import db, Jwk as JwkModel
-from json import dumps as json_encode
-from json import loads as json_decode
-from jwcrypto import jwt, jwk
+from itsdangerous import TimedJSONWebSignatureSerializer
+from os import urandom
 
 
 class Jwt:
-    def __init__(self, token=None):
-        self._token = token
-
+    def __init__(self):
         if not JwkModel.query.count():
             self.__create_key()
         else:
             self.__get_key()
 
     def create_token(self, data):
-        enc_jwt = jwt.JWT(header={"alg": "HS256"}, claims=data)
-        enc_jwt.make_signed_token(json_encode(self.__key))
-        return enc_jwt.serialize()
+        jwt = TimedJSONWebSignatureSerializer(self.__key, expires_in=600)
+        return jwt.dumps(data)
 
-    def get_token_data(self):
-        key = jwk.JWK(**self.__key)
-        dec_jwt = jwt.JWT().deserialize(jwt=self._token, key=key)
-        return dec_jwt.claim
-
-    def validate_token(self):
-        key = jwk.JWK(**self.__key)
-        dec_jwt = jwt.JWT().deserialize(jwt=self._token, key=key)
-        return dec_jwt.token.is_valid
+    def get_token_data(self, token):
+        jwt = TimedJSONWebSignatureSerializer(self.__key, expires_in=600)
+        return jwt.loads(token)
 
     def __create_key(self):
-        jwk_key = jwk.JWK(generate='oct', size=256)
-        self.__key = json_decode(jwk_key.export())
+        self.__key = urandom(24).hex()
 
-        jwk_model = JwkModel(self.__key['k'], self.__key['kty'])
+        jwk_model = JwkModel(self.__key)
+
         db.session.add(jwk_model)
         db.session.commit()
         return
