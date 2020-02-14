72cad3c0af17c2f77ffce7dd5b300b69fcc8bf6d
Thalles Koester <thallesdella@gmail.com>
2020-02-14 01:43:56 -0300

    Accepting parameters to initialise app

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,10 @@
 from apicefet import create_app
+from sys import argv
 import os
 
-app = create_app()
-
 if __name__ == "__main__":
+    env = 'develop' if len(argv) == 1 else argv[1]
+    app = create_app(env)
+
     port = int(os.environ.get('PORT', app.config['SERVER_PORT']))
     app.run(debug=app.config['DEBUG'], host=app.config['SERVER_IP'], port=port)
